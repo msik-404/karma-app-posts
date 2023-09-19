3ed121b9f3c111fc378c6baf96b620e71cc4a984
SIIIKOR <siiikor.sikorski@gmail.com>
2023-09-19 14:36:41 +0200

    String to ObjectID

--- src/main/java/com/msik404/karmaappposts/image/ImageRepository.java
+++ src/main/java/com/msik404/karmaappposts/image/ImageRepository.java
@@ -1,7 +1,8 @@
 package com.msik404.karmaappposts.image;
 
 import com.msik404.karmaappposts.image.repository.CustomImageRepository;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ImageRepository extends MongoRepository<ImageDocument, String>, CustomImageRepository {
+public interface ImageRepository extends MongoRepository<ImageDocument, ObjectId>, CustomImageRepository {
 }
