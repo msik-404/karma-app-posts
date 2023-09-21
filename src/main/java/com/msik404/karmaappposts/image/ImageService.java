a7fd18235ede5183350e2209dc8a3d15c5b8f389
SIIIKOR <siiikor.sikorski@gmail.com>
2023-09-21 21:54:44 +0200

    Implemented imageData compression to jpeg

---
+++ src/main/java/com/msik404/karmaappposts/image/ImageService.java
@@ -0,0 +1,41 @@
+package com.msik404.karmaappposts.image;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.msik404.karmaappposts.image.exception.FileProcessingException;
+import com.msik404.karmaappposts.image.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.Binary;
+import org.bson.types.ObjectId;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageRepository repository;
+
+    public void save(@NonNull ObjectId postId, byte[] imageData) throws FileProcessingException {
+
+        try {
+            var bufferedImage = ImageIO.read(new ByteArrayInputStream(imageData));
+            if (bufferedImage == null) {
+                throw new FileProcessingException();
+            }
+
+            var byteArrayOutputStream = new ByteArrayOutputStream();
+
+            ImageIO.write(bufferedImage, "jpeg", byteArrayOutputStream);
+
+            repository.save(new ImageDocument(postId, new Binary(byteArrayOutputStream.toByteArray())));
+        } catch (IOException ex) {
+            throw new FileProcessingException();
+        }
+    }
+
+}
---
+++ src/main/java/com/msik404/karmaappposts/image/exception/FileProcessingException.java
@@ -0,0 +1,8 @@
+package com.msik404.karmaappposts.image.exception;
+
+public class FileProcessingException extends RuntimeException {
+
+    public FileProcessingException() {
+        super("File could not be processed for some reason");
+    }
+}
