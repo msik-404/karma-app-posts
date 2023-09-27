3936d631587f9633c3a860123d3cbff180beae64
SIIIKOR <siiikor.sikorski@gmail.com>
2023-09-27 23:55:51 +0200

    Grpc server implemented

--- src/main/java/com/msik404/karmaappposts/KarmaAppPostsApplication.java
+++ src/main/java/com/msik404/karmaappposts/KarmaAppPostsApplication.java
@@ -1,12 +1,30 @@
 package com.msik404.karmaappposts;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.msik404.karmaappposts.grpc.impl.PostsGrpcImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class KarmaAppPostsApplication {
+
+    @Value("${KarmaAppPosts.grpc.default.threadAmount}")
+    private int threadPoolSize;
+
+    @Value("${KarmaAppPosts.grpc.default.port}")
+    private int defaultGrpcPort;
+
+    private final PostsGrpcImpl grpcImpl;
 
     public static void main(String[] args) {
         SpringApplication.run(KarmaAppPostsApplication.class, args);
@@ -16,6 +34,17 @@ public class KarmaAppPostsApplication {
     public CommandLineRunner runner() {
         return (args) -> {
             System.out.println("GRPC SERVER WILL RUN HERE");
+
+            ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(threadPoolSize);
+
+            Server server = ServerBuilder
+                    .forPort(defaultGrpcPort)
+                    .executor(threadPoolExecutor)
+                    .addService(grpcImpl)
+                    .build();
+
+            server.start();
+            server.awaitTermination();
         };
     }
 
