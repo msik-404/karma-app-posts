8d2d53244c6e362c71afa2f25f175027540ccb02
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-16 17:08:32 +0200

    Implemented RatingService methods for RatingRepository for easier error handling

--- src/main/java/com/msik404/karmaappposts/rating/RatingService.java
+++ src/main/java/com/msik404/karmaappposts/rating/RatingService.java
@@ -8,6 +8,7 @@ import com.msik404.karmaappposts.post.dto.FindParametersDto;
 import com.msik404.karmaappposts.post.order.PostDocRetrievalOrderStrategy;
 import com.msik404.karmaappposts.post.position.PostDocScrollPositionConcrete;
 import com.msik404.karmaappposts.rating.dto.IdAndIsPositiveOnlyDto;
+import com.msik404.karmaappposts.rating.exception.RatingNotFoundException;
 import com.msik404.karmaappposts.rating.repository.RatingRepository;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -20,6 +21,24 @@ import org.springframework.stereotype.Service;
 public class RatingService {
 
     private final RatingRepository ratingRepository;
+
+    @NonNull
+    public IdAndIsPositiveOnlyDto findByPostIdAndUserId(
+            @NonNull final ObjectId postId,
+            @NonNull final ObjectId userId) throws RatingNotFoundException {
+
+        return ratingRepository.findByPostIdAndUserId(postId, userId).orElseThrow(RatingNotFoundException::new);
+    }
+
+    public void findAndSetIsPositiveById(
+            @NonNull final ObjectId postId,
+            final boolean isPositive) throws RatingNotFoundException {
+
+        final long affectedDocs = ratingRepository.findAndSetIsPositiveById(postId, isPositive);
+        if (affectedDocs == 0) {
+            throw new RatingNotFoundException();
+        }
+    }
 
     @NonNull
     public List<IdAndIsPositiveOnlyDto> findFirstN(
