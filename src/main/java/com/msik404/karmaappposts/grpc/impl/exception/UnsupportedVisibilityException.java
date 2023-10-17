51f3aa384d7098b9b9c99bfe5441f3be6d2ededa
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-17 21:55:43 +0200

    Forgot to make this class extend from EncodableGrpcStatusException

--- src/main/java/com/msik404/karmaappposts/grpc/impl/exception/UnsupportedVisibilityException.java
+++ src/main/java/com/msik404/karmaappposts/grpc/impl/exception/UnsupportedVisibilityException.java
@@ -1,24 +1,16 @@
 package com.msik404.karmaappposts.grpc.impl.exception;
 
-import com.msik404.karmaappposts.encoding.EncodableException;
-import com.msik404.karmaappposts.encoding.ExceptionEncoder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import org.springframework.lang.NonNull;
 
-public class UnsupportedVisibilityException extends RuntimeException implements EncodableException, GrpcStatusException {
+public class UnsupportedVisibilityException extends EncodableGrpcStatusException {
 
     private final static String Id = "UnsupportedVisibility";
     private final static String ERROR_MESSAGE = "Unsupported visibility provided.";
 
     public UnsupportedVisibilityException() {
         super(ERROR_MESSAGE);
-    }
-
-    @NonNull
-    @Override
-    public String getEncodedException() {
-        return ExceptionEncoder.encode(getExceptionId(), ERROR_MESSAGE);
     }
 
     @NonNull
